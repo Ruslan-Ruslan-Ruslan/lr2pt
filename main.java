87a028d1a584bc2763a4219b8880f6f87b856a0c
Екатерина Хазнаферова <lashkoruslan@gmail.com>
2024-10-13 17:55:56 +0300

    Додано оголошення змінних

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class main {
+    public static void main(String[] args) {
+        long l = 123456L;     
+        double d1, d2;        
+        float f = 14.75f;     
+        char c1 = '0';        
+
+        System.out.println("Початкове значення l: " + l);
+        System.out.println("Початкове значення f: " + f);
+        System.out.println("Початкове значення c1: " + c1);
+    }
+}
